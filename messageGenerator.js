fca0a55176789cc1a071be6ec9a1dfa2bd6a3f5f
Alex K <kuzielalex@gmail.com>
2023-12-19 02:50:26 +0300

    changed project around new understanding of instructions. fixed project.

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,38 +1,19 @@
+// const phrases = ["you will have a great day", "maybe tomorrow will be better"]
+const num = Math.floor(Math.random()* 3)
 function messageGenerator() {
-    for (let i = 0; i <= Math.floor(Math.random() * 10); i++) {
-        if (i === 1) {
-                console.log("your day will be great")
-        }else if (i === 2) {
-                console.log("better day tomorrow")
-        }else if (i === 3) {
-            
-                console.log("your day will get better")
-        } else if (i === 4) {
-         
-                console.log("Try again tomorrow")
-        }else if (i === 5) {
-            
-                console.log("All your wishes will come true")
-        }else if (i === 6) {
+switch (num) {
+        case 1:
+                console.log("try agin")
         
-                console.log("Not today")
-        }else if (i === 7) {
+        break;
+        case 2:
+                console.log("youre getting better")
+        default:
+                console.log('fuck this shit')
         
-                console.log("You will do great things today")
-        }else if (i === 8) {
-        
-                console.log("Today is not your day, try again tomorrow")
-        }else if (i === 9) {
-    
-                console.log("Try again")
-        }else if (i === 10) {
-            
-                console.log("Your luck will be great")
-        }else {
-        
-                console.log("Try again")
-        }
-    }
-
 }
-// console.log(messageGenerator)
+}
+        
+
+
+console.log(messageGenerator())
