a42b26c6fdd785e7489243959b0b5d78047fa3c0
Alex K <kuzielalex@gmail.com>
2023-12-19 00:47:02 +0300

    first commit

---
+++ messageGenerator.js
@@ -0,0 +1,38 @@
+function messageGenerator() {
+    for (let i = 0; i <= Math.floor(Math.random() * 10); i++) {
+        if (i === 1) {
+                console.log("your day will be great")
+        }else if (i === 2) {
+                console.log("better day tomorrow")
+        }else if (i === 3) {
+            
+                console.log("your day will get better")
+        } else if (i === 4) {
+         
+                console.log("Try again tomorrow")
+        }else if (i === 5) {
+            
+                console.log("All your wishes will come true")
+        }else if (i === 6) {
+        
+                console.log("Not today")
+        }else if (i === 7) {
+        
+                console.log("You will do great things today")
+        }else if (i === 8) {
+        
+                console.log("Today is not your day, try again tomorrow")
+        }else if (i === 9) {
+    
+                console.log("Try again")
+        }else if (i === 10) {
+            
+                console.log("Your luck will be great")
+        }else {
+        
+                console.log("Try again")
+        }
+    }
+
+}
+// console.log(messageGenerator)
