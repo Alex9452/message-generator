1e4f0315897f9bf5b1677ffffc4c5cac63014c8f
Alex K <kuzielalex@gmail.com>
2023-12-19 19:54:37 +0300

    made functions work

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,6 +1,15 @@
-const number = Math.floor(Math.random() * 5)
+function randomNumber() { 
+        return Math.floor(Math.random() * 5)}
 const dailyFortune = {
         starSigns: ['Libra', 'scorpio', "gemini"],
         luck: ['good luck', "bad luck", "no luck"],
         quote: ['Youll have a great day', "keep your head up", "better luck tomorrow"]
 }
+
+function pickFortune() {
+        for (let i = randomNumber(); i < dailyFortune.starSigns.length; i++) {
+                return dailyFortune.starSigns[i]
+        }
+}
+
+console.log(pickFortune())
